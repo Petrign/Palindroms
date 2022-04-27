398d9bc3537296f4288fceb24e7d3b923aa55c2f
Petrign <Ignatovy_efr@mail.ru>
2022-04-27 20:05:55 +0300

    Fixed dichotomy

--- palindroms.cpp
+++ palindroms.cpp
@@ -20,8 +20,7 @@ bool tryoddpal(int *a, int cent, int width, int l)
 {
 	if (width > cent || width > l-cent-1) return false;
 	//Checker
-	return ispal(a+cent-width, 1+2*width);
-	
+	return ispal(a+cent-width, 1+2*width);	
 }
 
 bool tryevenpal(int *a, int cent, int width, int l)
@@ -29,6 +28,19 @@ bool tryevenpal(int *a, int cent, int width, int l)
 	if (width > cent || width > l-cent-1) return false;
 	//Checker
 	return ispal(a+cent-width, 2*width+2);
+}
+
+bool hasOddSubPal(int *a, int rad, int l)
+{
+	int maxMove = l-2*rad-1;
+	for (int move = 0; move <= maxMove; move++)
+	{
+		if (tryoddpal(a,move+rad, rad, l))
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 /*
@@ -43,28 +55,32 @@ int longestoddpal(int *a, int cent, int l) //Finding possible "Radius"
 }
 */ 
 
-int longestoddpal(int *a, int cent, int l) //Finding possible "Radius" using Dichotomy
+int longestoddpal(int *a, int l) //Finding possible "Radius" using Dichotomy
 {
-	int max = std::min(cent, l-cent-1);
+	int max = (l-1)/2;
 	int min = 0;
-	int cur = -1;
-	while (min!=max && cur != max && cur != min)
+	int cur = (min+max+1)/2;
+	while (min!=max)
 	{
-		cur = int(std::round((min+max)/2.0));
-		if (tryoddpal(a,cent,cur,l))
+		if (hasOddSubPal(a,cur,l))
 		{
 			min = cur;
+			cur = (min+max+1)/2;
 		}
 		else
 		{
 			max = cur;
+			cur = (min+max)/2;
 		}
-		cur = int(std::round((min+max)/2.0));
-		std::cout << "cur/min/max(odd)" << cur << " " << min << " " << max << " in " << cent  << "\n";
+		if(cur == 0)
+		{
+			return 0;
+		}
 	}
 	return cur;
 }
 
+/*
 int longestevenpal(int *a, int cent, int l) //Finding possible "Radius"
 {
 	int max = std::min(cent, l-cent-1);
@@ -86,7 +102,8 @@ int longestevenpal(int *a, int cent, int l) //Finding possible "Radius"
 	}
 	return cur;
 }
-
+*/
+/*
 int findevenpal(int *a, int l) //Finding center
 {
 	int max = 0;
@@ -123,7 +140,7 @@ int findpal(int *a, int l) //Finding maximum
 {
 	return std::max(findevenpal(a, l),findoddpal(a,l));
 }
-
+*/
 
 
 
@@ -250,8 +267,7 @@ int main()
 		std::cout << carmovement(b,a,n,calcCarInterLength(m,n)) << "\n";
 	}
 	*/
-	if (b[0] == b[1]) std::cout << findpal(a,n);
-	else std::cout << "3";
+	std::cout << longestoddpal(a,n)*2+1;
 	return 0;
 }
 
